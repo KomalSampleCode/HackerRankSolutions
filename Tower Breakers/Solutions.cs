d7d74b4b30e26b5241ad36bb427fe7ad7ce4d682
Komal Raval <150650603+KomalSampleCode@users.noreply.github.com>
2023-11-21 13:15:39 +0530

    Create Solutions.cs

---
+++ Tower Breakers/Solutions.cs
@@ -0,0 +1,67 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'towerBreakers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+
+    public static int towerBreakers(int n, int m)
+    {
+        if (n == 1) { // if only one tower is there, tower 2 will never get chance. so one wins
+            return 1;
+        } else if (m == 1) { // if tower's height is 1 then it can't make a move so two wins.
+            return 2;
+        }
+        else if (n % 2 == 0) { // whatever move one will make the second player will also take so one loses first.
+            return 2;
+        } else { // due to odd numbers, even number wouldn't be able to take the test. so it fails and one wins.
+            return 1;
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.towerBreakers(n, m);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
